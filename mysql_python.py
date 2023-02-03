1cd171f1757a2aa1cd154bfa5fb128325f8b7964
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2023-02-03 19:48:34 +0200

    Created files

---
+++ mysql_python.py
@@ -0,0 +1,11 @@
+# 1. Створити нову базу даних 'my_first-db'.
+# 2. Створити в цій базі даних таблицi:
+# 'student' з полями: 'id' (INT) i 'name' (VARCHAR(255));
+# 'employee' з полями: 'id' (INT_AUTO_INCREMENT PRIMARY KEY) ,
+# 'name' (VARCHAR(255)) i 'salary' (INT(6))
+# 3. Змінити у таблиці 'student' поле id' PRIMARY KEY.
+# 4. Додати до таблиць:
+# 'student' - (01, 'John');
+# 'employee' - (01, 'John', 1000)
+
+
