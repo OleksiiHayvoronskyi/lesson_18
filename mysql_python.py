b88da17bb0cb2713058c2698a6b3ddd8532404d9
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2023-02-03 22:52:18 +0200

    Done: created Database and tables, added column to tables

--- mysql_python.py
+++ mysql_python.py
@@ -1,11 +1,60 @@
-# 1. Створити нову базу даних 'my_first-db'.
+import mysql.connector
+
+# ЗАВДАННЯ:
+# 1. Створити нову базу даних 'my_first_db'.
 # 2. Створити в цій базі даних таблицi:
-# 'student' з полями: 'id' (INT) i 'name' (VARCHAR(255));
-# 'employee' з полями: 'id' (INT_AUTO_INCREMENT PRIMARY KEY) ,
+#       'student' з полями: 'id' (INT) i 'name' (VARCHAR(255));
+#       'employee' з полями: 'id' (INT_AUTO_INCREMENT PRIMARY KEY) ,
+#       'name' (VARCHAR(255)) i 'salary' (INT(6))
+# 3. Змінити у таблиці 'student' поле 'id' на PRIMARY KEY.
+# 4. Додати до таблиці 'student' такі дані: (01, 'John').
+# 5. Додати до таблиці 'employee' такі дані:  (01, 'John', 1000).
+
+
+# Створюю коннектор.
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Oleksiisql1985',
+    database='my_first_db'
+)
+
+
+# 1. Створюю базу даних 'my_first_db'.
+mycursor = mydb.cursor()
+mycursor.execute('CREATE DATABASE my_first_db')
+
+
+# 2. Створюю таблицю 'student' з полями: 'id' (INT) i 'name' (VARCHAR(255)).
+mycursor = mydb.cursor()
+mycursor.execute('CREATE TABLE student (id INT, name VARCHAR(255))')
+
+
+# 2. Створюю таблицю 'employee' з полями:'id' (INT_AUTO_INCREMENT PRIMARY KEY),
 # 'name' (VARCHAR(255)) i 'salary' (INT(6))
-# 3. Змінити у таблиці 'student' поле id' PRIMARY KEY.
-# 4. Додати до таблиць:
-# 'student' - (01, 'John');
-# 'employee' - (01, 'John', 1000)
+mycursor = mydb.cursor()
+mycursor.execute('CREATE TABLE employee (id INT AUTO_INCREMENT PRIMARY KEY, '
+                 'name VARCHAR(255), salary INT(6))')
 
 
+# 3. Змінюю у таблиці 'student' поле 'id' на PRIMARY KEY.
+mycursor = mydb.cursor()
+mycursor.execute('ALTER TABLE student DROP id, '
+                 'ADD id INT AUTO_INCREMENT PRIMARY KEY')
+
+
+# 4. Додаю до таблиці 'student' такі дані: (01, 'John').
+mycursor = mydb.cursor()
+sql = 'INSERT INTO student (id, name) VALUES (%s, %s)'
+val = (1, 'John')
+mycursor.execute(sql, val)
+
+
+# 5. Додаю до таблиці 'employee' такі дані: (01, 'John', 1000).
+mycursor = mydb.cursor()
+sql = 'INSERT INTO employee (id, name, salary) VALUES (%s, %s, %s)'
+val = (1, 'John', 1000)
+mycursor.execute(sql, val)
+
+mydb.commit()
+print(mycursor.rowcount, 'record inserted.')
